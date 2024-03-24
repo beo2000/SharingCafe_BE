e240031c1004d7c2f6af5fd0434073466580bd20
Duchnh <hduy01012000@gmail.com>
2024-03-24 13:55:44 +0700

    Update response

--- APP/DAL/chatDAL.js
+++ APP/DAL/chatDAL.js
@@ -30,38 +30,28 @@ export async function saveMessage(messageId, messageData) {
 export async function getChatHistory(userIdFrom, userIdTo, limit, offset) {
   const sqlQuery = `
 SELECT 
-  message_id
-  , sender_id
-  , sender.user_name as sender_name
-  , sender.profile_avatar as sender_avatar
-  , receiver_id
-  , receiver.user_name as receiver_name
-  , receiver.profile_avatar as receiver_avatar
-  , "content"
-  , created_at
+  m.message_id,
+  m.sender_id,
+  sender.user_name AS sender_name,
+  sender.profile_avatar AS sender_avatar,
+  m.receiver_id,
+  receiver.user_name AS receiver_name,
+  receiver.profile_avatar AS receiver_avatar,
+  m.content,
+  m.created_at
 FROM 
   public.message m
-inner join 
-  public."user" sender
-  on 1 = 1
-    and sender.user_id = m.sender_id
-inner join 
-  public."user" receiver
-  on 1 = 1
-   and receiver.user_id = m.receiver_id
-    WHERE 1 = 1
-    and( 
-      or sender_id = '${userIdFrom}'
-      or sender_id = '${userIdFrom}'
-    )
-    and (
-      or receiver_id = '${userIdTo}'
-      or receiver_id = '${userIdTo}'
-    )
+INNER JOIN 
+  public."user" sender ON sender.user_id = m.sender_id
+INNER JOIN 
+  public."user" receiver ON receiver.user_id = m.receiver_id
+WHERE 
+  (m.sender_id = '${userIdFrom}' OR m.receiver_id = '${userIdFrom}')
+  AND (m.sender_id = '${userIdTo}' OR m.receiver_id = '${userIdTo}')
 ORDER BY 
-  created_at
-    LIMIT ${limit}
-    OFFSET ${offset}
+  m.created_at
+LIMIT ${limit}
+OFFSET ${offset};
     `;
   const result = await SequelizeInstance.query(sqlQuery, {
     type: SequelizeInstance.QueryTypes.SELECT,
