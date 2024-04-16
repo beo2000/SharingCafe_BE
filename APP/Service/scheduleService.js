e75f2fe77fc55c6c423cb0e591dff0bda24a6a8b
Duchnh <hduy01012000@gmail.com>
2024-04-17 02:39:07 +0700

    Send noti with schedule updated

--- APP/Service/scheduleService.js
+++ APP/Service/scheduleService.js
@@ -1,4 +1,5 @@
 import * as scheduleDAL from '../DAL/scheduleDAL.js';
+import * as firebaseHelper from '../utility/FirebaseHelper.js';
 import { v4 as uuidv4 } from 'uuid';
 
 export async function createSchedule(dataObj) {
@@ -10,6 +11,18 @@ export async function getScheduleBetweenUsers(userId, anotherUserId) {
 }
 
 export async function changeStatus(dataObj) {
+  const [schedule] = await scheduleDAL.getScheduleId(dataObj.schedule_id);
+  const tile = `SCHEDULE HAS BEEN UPDATED `;
+  await firebaseHelper.sendNotification(
+    schedule.user_from_token,
+    tile,
+    `DEAR ${schedule.user_from}` + schedule.message,
+  );
+  await firebaseHelper.sendNotification(
+    schedule.user_to_token,
+    tile,
+    `DEAR ${schedule.user_to}` + schedule.message,
+  );
   return await scheduleDAL.changeStatus(dataObj);
 }
 
