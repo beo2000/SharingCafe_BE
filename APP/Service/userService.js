dd49f6a3ba3170956e5a86dfbf98a35b7ff76d64
HOANG Trung Thong <thoang5@mantu.com>
2024-04-19 16:46:08 +0700

    change file name

--- APP/Service/userService.js
+++ APP/Service/userService.js
@@ -2,7 +2,7 @@ import { Error } from 'sequelize';
 import * as userDAL from '../DAL/userDAL.js';
 import * as matchDAL from '../DAL/matchDAL.js';
 import * as commonEnum from '../common/CommonEnums.js';
-import * as commonFunction from '../common/commonFunction.js';
+import * as commonFunction from '../common/CommonFunctions.js';
 import { v4 as uuidv4 } from 'uuid';
 import * as firebaseHelper from '../utility/FirebaseHelper.js';
 import * as notificationDAL from '../DAL/notificationDAL.js';
