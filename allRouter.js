0022cae29003572c2bbf2205cc944292e7d9cf21
Unknow <vuminhchau.12cxm@gmail.com>
2024-04-25 20:37:58 +0700

    Update report swagger

--- allRouter.js
+++ allRouter.js
@@ -2542,6 +2542,9 @@ router.post(
  *                 type: string
  *                 description: ID of the blog
  *                 example: "9d47a0e3-9f5c-4108-9985-ba52d467a6c2"
+ *               content:
+ *                 type: string
+ *                 description: reason to report
  *               report_status_id:
  *                 type: string
  *                 description: ID of the report status
@@ -2621,6 +2624,9 @@ router.post(
  *                 type: string
  *                 description: ID of the event
  *                 example: "50b415b6-b874-429c-9f31-56db62ff0c13"
+ *               content:
+ *                 type: string
+ *                 description: reason to report
  *               report_status_id:
  *                 type: string
  *                 description: ID of the report status
@@ -2677,6 +2683,9 @@ router.post(
  *                 type: string
  *                 description: ID of the user
  *                 example: "716ab41a-a01e-46ac-a907-f0c1419f212f"
+ *               content:
+ *                 type: string
+ *                 description: reason to report
  *               report_status_id:
  *                 type: string
  *                 description: ID of the report status
