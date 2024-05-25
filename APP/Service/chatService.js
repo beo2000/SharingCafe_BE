0c52e058c29c7baf09f6b28cc986aa46092035c8
Duchnh <hduy01012000@gmail.com>
2024-05-25 16:01:31 +0700

    revert

--- APP/Service/chatService.js
+++ APP/Service/chatService.js
@@ -6,21 +6,14 @@ import { v4 as uuidv4 } from 'uuid';
 
 export async function saveMessage(messageData) {
   const messageId = uuidv4();
-  // const block = await userDAL.getBlockCouple(messageData);
-  // console.log(block);
-  // if (block.length == 0) {
-  //   return null; // If blocked, return null
-  // }
-  if (messageData.appointment == null) {
-    const { from, to, message } = messageData;
-    const [userFrom] = await userDAL.getUserInfoById(from);
-    const [userTo] = await userDAL.getUserInfoById(to);
+  const { from, to, message } = messageData;
+  const [userFrom] = await userDAL.getUserInfoById(from);
+  const [userTo] = await userDAL.getUserInfoById(to);
 
-    const title = `${userFrom.user_name} đã gửi tin nhắn cho bạn!`;
-    const body = `${message}`;
-    firebaseHelper.sendNotification(userTo.token_id, title, body);
-    await chatDAL.saveMessage(messageId, messageData);
-  }
+  const title = `${userFrom.user_name} đã gửi tin nhắn cho bạn!`;
+  const body = `${message}`;
+  firebaseHelper.sendNotification(userTo.token_id, title, body);
+  await chatDAL.saveMessage(messageId, messageData);
   return messageId;
 }
 export async function getChatHistory(userIdFrom, userIdTo, limit, offset) {
